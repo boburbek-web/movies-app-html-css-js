26d419c98184d23f3f6c4f86aab8608e96561925
Utkirov Boburbek <boburbekutkirov10@gmail.com>
2023-04-07 14:48:31 +0500

    moviesApp

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
-// Get initial movies
 getMovies(api.url)
 
 async function getMovies(url) {
